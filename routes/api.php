5eb88915e26a2ba6e36c91bf390e2b9c781e4995
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2026-01-22 22:15:25 +0700

    refactor: change middleware in api route group

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'userData']);
 });
